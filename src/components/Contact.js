e574a7d590de1ae0e55152e425e40c880fdea2d6
PaulCrtr <paul.th.cartier@gmail.com>
2020-03-30 12:24:34 +0200

    corrections

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './style.css'
 function Contact(){
     return <section className="Contact">
-        <img src="https://randomuser.me/api/portraits/men/70.jpg" alt="test"></img>
+        <img className="avatar" src="https://randomuser.me/api/portraits/men/70.jpg" alt="test"></img>
         <div>
         <h4>Arnold Robinson</h4>
         <div  className="status">
         <div className="status-online"></div>
-        <p>Online</p>
+        <p className="status-text">Online</p>
         </div>
         </div>
     </section>
