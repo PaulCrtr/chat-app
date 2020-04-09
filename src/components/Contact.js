1f8a00c096ad06f7fd88f3ee8c2289fc3a950cc7
PaulCrtr <paul.th.cartier@gmail.com>
2020-04-09 11:09:17 +0200

    state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,27 +2,41 @@ import React from "react";
 import "./style.css";
 import PropTypes from "prop-types";
 
-function Contact(props) {
-  return (
-    <section className="Contact">
-      <img className="avatar" src={props.avatar} alt="test"></img>
-      <div>
-        <h4 className="name">{props.name}</h4>
-        <div className="status">
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { online: this.props.online };
+  }
+  render() {
+    return (
+      <section className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="test"></img>
+        <div>
+          <h4 className="name">{this.props.name}</h4>
           <div
-            className={props.online ? "status-online" : "status-offline"}
-          ></div>
-          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+            onClick={(event) => {
+              const newStatus = !this.state.online;
+              this.setState({ online: newStatus });
+            }}
+            className="status"
+          >
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
+            ></div>
+            <p className="status-text">
+              {this.state.online ? "Online" : "Offline"}
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 Contact.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  online: PropTypes.bool
+  online: PropTypes.bool,
 };
 
 export default Contact;
